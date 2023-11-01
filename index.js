456d53222aa8d7ce019d98c78382c4cb2045a6c7
TanzilaMomin <147149600+TanzilaMomin@users.noreply.github.com>
2023-11-01 15:04:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function handleAdd(h,w,clr){
+    document.getElementById('show').style.width=h+"px";
+    document.getElementById('show').style.height=w+"px"
+    document.getElementById('show').style.backgroundColor=clr;
+   }
